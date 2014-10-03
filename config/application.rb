fd4165c787274db189a3f388c42f6995fa4f2b7b
Charlie Gaines <beaugaines@gmail.com>
2014-10-03 14:07:29 -0400

    disable test::unit

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Blocitoff
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # disable Test::Unit
+    config.generators do |g|
+        g.test_framework nil
+    end
   end
 end
