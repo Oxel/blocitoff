6ea0cdcde518bfa00063323b768eaa79192423c1
Charlie Gaines <beaugaines@gmail.com>
2014-10-03 14:47:24 -0400

    install bcrypt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user
+
+  protected
+
+  def authenticate_user
+    redirect_to sign_up_path unless current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'todos#index'
-  
+
+  get 'sign_up', to: 'users#new', as: 'sign_up'
+
   resources :users do
     resources :todos
   end
