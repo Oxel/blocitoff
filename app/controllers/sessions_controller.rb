c2a48cfa75f18d4bfbb96e51a85ecbcc74ce74ac
Charlie Gaines <beaugaines@gmail.com>
2014-10-06 10:05:46 -0400

    user auth half done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def destroy
+    session[:user] = nil
+    redirect_to root_url, notice: 'Logged out'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
   root 'todos#index'
 
   get 'sign_up', to: 'users#new', as: 'sign_up'
+  get 'sign_in', to: 'sessions#new', as: 'sign_in'
+  get 'sign_out', to: 'sessions#destroy', as: 'sign_out'
 
   resources :users do
     resources :todos
